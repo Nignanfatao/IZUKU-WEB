063f09831dbda0e61743875e7fa659889616833e
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-01-28 10:45:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -38,33 +38,33 @@ app.use("/", async (req, res) => {
   async function ovl() {
     const { state, saveCreds } = await useMultiFileAuthState(authInfoPath);
     try {
-      let ex = OvlWASocket({ 
+      let ovl = OvlWASocket({ 
         printQRInTerminal: false,
         logger: pino({ level: "silent" }), 
         browser: Browsers.baileys("Desktop"),
         auth: state 
       });
 
-      ex.ev.on("connection.update", async (s) => {
+      ovl.ev.on("connection.update", async (s) => {
         const { connection, lastDisconnect, qr } = s;
         if (qr) { res.end(await toBuffer(qr)); }
 
         if (connection == "open"){
           await delay(3000);
-          let user = ex.user.id;
+          let user = ovl.user.id;
 
           let CREDS = fs.readFileSync(authInfoPath + '/creds.json')
           var Scan_Id = Buffer.from(CREDS).toString('base64')
-
-          let msgsss = await ex.sendMessage(user, { text: `Ovl;;; ${Scan_Id}` });
-          await ex.sendMessage(user, { text: MESSAGE }, { quoted: ms });
+          let mi = "Merci d'avoir choisie ovl-Md";
+          let msgsss = await ovl.sendMessage(user, { text: `Ovl;;; ${Scan_Id}` });
+          await ovl.sendMessage(user,{image: {url: "https://telegra.ph/file/0d81626ca4a81fe93303a.jpg", caption: mi }, { quoted: ms });
           await delay(1000);
           try {
               await fs.emptyDirSync(authInfoPath);
           } catch (e) {}
         }
 
-        ex.ev.on('creds.update', saveCreds);
+        ovl.ev.on('creds.update', saveCreds);
 
         if (connection === "close") {            
           let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
