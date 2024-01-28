7bb6698cd6d0a58da80dbc323e05f7b97fbf4848
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-01-28 09:19:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,94 +1,98 @@
+const fs = require("fs-extra");
 const express = require("express");
 const app = express();
 const pino = require("pino");
 const { toBuffer } = require("qrcode");
-const path = require('path');
-const fs = require("fs-extra");
-const { Boom } = require("@hapi/boom");
 
-const PORT = process.env.PORT || "5000";
-const MESSAGE = process.env.MESSAGE || "Merci d'avoir choisi Ovl-Md";
+const PORT = process.env.PORT || 5000;
 
-if (fs.existsSync('./auth_info_baileys')) {
-  fs.emptyDirSync(__dirname + '/auth_info_baileys');
+const authInfoPath = __dirname + '/auth_info_baileys';
+
+// Vérifier si le répertoire existe déjà
+if (!fs.existsSync(authInfoPath)) {
+  try {
+    // Créer le répertoire
+    fs.mkdirSync(authInfoPath);
+    console.log('Répertoire auth_info_baileys créé avec succès.');
+  } catch (error) {
+    console.error('Erreur lors de la création du répertoire auth_info_baileys :', error);
+  }
+} else {
+  console.log('Le répertoire auth_info_baileys existe déjà.');
+}
+
+// Utilisez fs.emptyDirSync après avoir vérifié ou créé le répertoire
+try {
+  fs.emptyDirSync(authInfoPath);
+  console.log('Contenu du répertoire auth_info_baileys vidé avec succès.');
+} catch (error) {
+  console.error('Erreur lors du vidage du répertoire auth_info_baileys :', error);
 }
 
 app.use("/", async (req, res) => {
-  try {
-    const { default: suhailWASocket, useMultiFileAuthState, Browsers, delay, DisconnectReason, makeInMemoryStore } = require("@sampandey001/baileys");
-    const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+  const { default: OvlWASocket, useMultiFileAuthState, Browsers, delay, DisconnectReason, makeInMemoryStore } = require("@sampandey001/baileys");
 
-    async function ovl() {
-      const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys');
-      try {
-        let ovl = suhailWASocket({ 
-          printQRInTerminal: false,
-          logger: pino({ level: "silent" }), 
-          browser: Browsers.baileys("Desktop"),
-          auth: state 
-        });
+  const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) });
 
-        ovl.ev.on("connection.update", async (s) => {
-          const { connection, lastDisconnect, qr } = s;
-          if (qr) { 
-            res.end(await toBuffer(qr));
+  async function ovl() {
+    const { state, saveCreds } = await useMultiFileAuthState(authInfoPath);
+    try {
+      let ex = OvlWASocket({ 
+        printQRInTerminal: false,
+        logger: pino({ level: "silent" }), 
+        browser: Browsers.baileys("Desktop"),
+        auth: state 
+      });
+
+      ex.ev.on("connection.update", async (s) => {
+        const { connection, lastDisconnect, qr } = s;
+        if (qr) { res.end(await toBuffer(qr)); }
+
+        if (connection == "open"){
+          await delay(3000);
+          let user = ex.user.id;
+
+          let CREDS = fs.readFileSync(authInfoPath + '/creds.json')
+          var Scan_Id = Buffer.from(CREDS).toString('base64')
+
+          let msgsss = await ex.sendMessage(user, { text: `Ovl;;; ${Scan_Id}` });
+          await ex.sendMessage(user, { text: MESSAGE }, { quoted: ms });
+          await delay(1000);
+          try {
+              await fs.emptyDirSync(authInfoPath);
+          } catch (e) {}
+        }
+
+        ex.ev.on('creds.update', saveCreds);
+
+        if (connection === "close") {            
+          let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
+          if (reason === DisconnectReason.connectionClosed) {
+            console.log("Connection fermée");
+          } else if (reason === DisconnectReason.connectionLost) {
+            console.log("Connection perdue depuis le serveur !");
+          } else if (reason === DisconnectReason.restartRequired) {
+            console.log("Redémarrage requis, redémarrage en cours...");
+            ovl().catch(err => console.log(err));
+          } else if (reason === DisconnectReason.timedOut) {
+            console.log("Connexion expirée !");
+          }  else {
+            console.log('Connexion fermée avec le bot. Veuillez exécuter à nouveau.');
+            console.log(reason);
           }
-
-          if (connection === "open") {
-            await delay(3000);
-            let user = ovl.user.id;
-
-            let CREDS = fs.readFileSync(__dirname + '/auth_info_baileys/creds.json')
-            var Scan_Id = Buffer.from(CREDS).toString('base64')
-
-            console.log(`
-====================  SESSION ID  ==========================                   
-SESSION-ID ==> ${Scan_Id}
--------------------   SESSION CLOSED   -----------------------
-`)
-
-            let msgsss = await ovl.sendMessage(user, { text: `Ovl;;; ${Scan_Id}` });
-            await ovl.sendMessage(user, { text: MESSAGE }, { quoted: ms });
-            await delay(1000);
-            try {
-              await fs.emptyDirSync(__dirname + '/auth_info_baileys');
-            } catch (e) {}
-
-            ovl.ev.on('creds.update', saveCreds)
-          }
-
-          if (connection === "close") {            
-            let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
-
-            if (reason === DisconnectReason.connectionClosed) {
-              console.log("Connection fermée");
-            } else if (reason === DisconnectReason.connectionLost) {
-              console.log("Connexion perdue avec le serveur !");
-            } else if (reason === DisconnectReason.restartRequired) {
-              console.log("Redémarrage requis, redémarrage...");
-              ovl().catch(err => console.log(err));
-            } else if (reason === DisconnectReason.timedOut) {
-              console.log("Connexion expirée !");
-            } else {
-              console.log('Connexion fermée avec le bot. Veuillez relancer.');
-              console.log(reason);
-            }
-          }
-        });
-      } catch (err) {
-        console.error('Erreur dans la fonction ovl :', err);
-        await fs.emptyDirSync(__dirname + '/auth_info_baileys'); 
-      }
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      await fs.emptyDirSync(authInfoPath);
     }
-
-    ovl().catch(async (err) => {
-      console.error('Erreur dans le traitement de la requête :', err);
-      await fs.emptyDirSync(__dirname + '/auth_info_baileys'); 
-    });
-  } catch (error) {
-    console.error('Erreur lors du traitement de la requête :', error);
-    res.status(500).send('Erreur lors du traitement de la requête.');
   }
+
+  ovl().catch(async (err) => {
+    console.log(err);
+    await fs.emptyDirSync(authInfoPath);
+    // MADE WITH
+  });
 });
 
-app.listen(PORT, () => console.log(`App écoutée sur http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`App écoutée sur le port http://localhost:${PORT}`));
