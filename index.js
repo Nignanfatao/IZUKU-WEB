ce5ff486256dc903ac1b03402314eb7960dc494f
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-03-06 03:58:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use("/", async (req, res) => {
           let CREDS = fs.readFileSync(authInfoPath + '/creds.json')
           var Scan_Id = Buffer.from(CREDS).toString('base64');
           let msgsss = await ovl.sendMessage(user, { text: `Ovl;;; ${Scan_Id}` });
-          await ovl.sendMessage(user,{image: {url: "https://telegra.ph/file/0d81626ca4a81fe93303a.jpg"}, caption: "Merci d'avoir choisie ovl-Md"}, { quoted: ms });
+          await ovl.sendMessage(user, {image: {url: "https://telegra.ph/file/0d81626ca4a81fe93303a.jpg"}, caption: "Merci d'avoir choisie ovl-Md"});
           await delay(1000);
           try {
               await fs.emptyDirSync(authInfoPath);
